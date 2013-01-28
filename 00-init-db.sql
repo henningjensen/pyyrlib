afbff27d5053807e195d77e296cd57eabf9b2ea1
Henning Jensen <henning.jensen@gmail.com>
2013-01-28 20:50:25 +0100

    Fix problem with duplicate location entries.

--- 00-init-db.sql
+++ 00-init-db.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS `locations` (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   `name` VARCHAR(100) NOT NULL,
   `country` VARCHAR(100) NOT NULL,
-  `xml` VARCHAR(200) NOT NULL
+  `xml` VARCHAR(200) NOT NULL,
+  UNIQUE (name, country) ON CONFLICT REPLACE
 );
 
 CREATE INDEX locations_name ON locations ('name');
